5d523b8f99d1365be8c6d92005b806916850bd63
ajiang10 <aj3242@columbia.edu>
2026-04-27 00:27:31 -0400

    panel regressions edits

--- Code/3_Panel_Methods.R
+++ Code/3_Panel_Methods.R
@@ -2,18 +2,18 @@ library(tidyverse)
 library(plm)
 
 # Load panel
-panel_data <- readRDS("Data/master_panel.rds") %>%
+master_panel <- readRDS("Data/master_panel.rds") %>%
   filter(Year <= 2020) # For panel methods, we focus on 2002-2020 to avoid complications with post-treatment periods in the later years
 
 # Run pooled OLS
-Pooled_model <- plm(lnGDP_Per_Capita ~ new_mw_wind + new_mw_solar + new_mw_gas + pct_white + pct_black + pct_asian + pct_poverty + pct_assoc + pct_bach + pct_masters + pct_farmer + pct_elderly + pct_under18 + pct_maleemploy + pct_femaleemploy + natamen + dist_to_urban_25k + dist_to_urban_100k + dist_to_urban_250k + dist_to_urban_500k + dist_to_urban_1mil, 
+Pooled_model <- plm(lnGDP_Per_Capita ~ new_mw_wind + new_mw_solar + new_mw_gas + pct_white + pct_black + pct_poverty + pct_assoc + pct_bach + pct_masters + pct_farmer + pct_elderly + pct_under18 + pct_maleemploy + pct_femaleemploy + dist_to_urban_100k + dist_to_urban_250k + dist_to_urban_500k,
                 data = master_panel, 
                 index = c("GeoFIPS", "Year"), 
                 model = "pooling")
 summary(Pooled_model)
 
 # Run FE
-FE_model <- plm(lnGDP_Per_Capita ~ new_mw_wind + new_mw_solar + new_mw_gas + pct_white + pct_black + pct_asian + pct_poverty + pct_assoc + pct_bach + pct_masters + pct_farmer + pct_elderly + pct_under18 + pct_maleemploy + pct_femaleemploy + natamen + dist_to_urban_25k + dist_to_urban_100k + dist_to_urban_250k + dist_to_urban_500k + dist_to_urban_1mil, 
+FE_model <- plm(lnGDP_Per_Capita ~ new_mw_wind + new_mw_solar + new_mw_gas + pct_white + pct_black + pct_poverty + pct_assoc + pct_bach + pct_masters + pct_farmer + pct_elderly + pct_under18 + pct_maleemploy + pct_femaleemploy + dist_to_urban_100k + dist_to_urban_250k + dist_to_urban_500k,
                 data = master_panel, 
                 index = c("GeoFIPS", "Year"), 
                 model = "within", 
@@ -23,7 +23,7 @@ summary(FE_model)
 
 
 # Run RE
-RE_model <- plm(lnGDP_Per_Capita ~ new_mw_wind + new_mw_solar + new_mw_gas + pct_white + pct_black + pct_asian + pct_poverty + pct_assoc + pct_bach + pct_masters + pct_farmer + pct_elderly + pct_under18 + pct_maleemploy + pct_femaleemploy + natamen + dist_to_urban_25k + dist_to_urban_100k + dist_to_urban_250k + dist_to_urban_500k + dist_to_urban_1mil, 
+RE_model <- plm(lnGDP_Per_Capita ~ new_mw_wind + new_mw_solar + new_mw_gas + pct_white + pct_black + pct_poverty + pct_assoc + pct_bach + pct_masters + pct_farmer + pct_elderly + pct_under18 + pct_maleemploy + pct_femaleemploy  + dist_to_urban_100k + dist_to_urban_250k + dist_to_urban_500k,
                 data = master_panel, 
                 index = c("GeoFIPS", "Year"), 
                 model = "random", 
